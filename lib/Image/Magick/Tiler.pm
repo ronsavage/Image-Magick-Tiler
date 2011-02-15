3bbda04a6b72e11a82dd02b44426c95acec6af9c
Ron Savage <ron@savage.net.au>
2011-02-15 16:58:12 +1100

    Replace /usr/bin/perl with /usr/bin/env perl and common::sense with use strict and use warnings

--- lib/Image/Magick/Tiler.pm
+++ lib/Image/Magick/Tiler.pm
@@ -57,7 +57,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 
 );
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 
 # -----------------------------------------------
 
@@ -247,7 +247,7 @@ C<Image::Magick::Tiler> - Slice an image into N x M tiles.
 
 =head1 Synopsis
 
-	#!/usr/bin/perl
+	#!/usr/bin/env perl
 
 	use Image::Magick::Tile;
 
